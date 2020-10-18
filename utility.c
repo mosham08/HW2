cf5b93413070f8c270f7f61408a1e7452f55cace
Mohammad <mohammad.abdellatif@mavs.uta.edu>
2020-10-18 18:10:25 -0500

    HW2 with drivers and utility

---
+++ utility.c
@@ -0,0 +1,35 @@
+/* Utility program, print time, name, PID, wait, print again, exit
+ */
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <locale.h>
+
+int main(void) {
+	time_t t;
+	char s[256];
+	int i, j;
+	pid_t p,pp;
+
+	clock_t begin = clock();
+   	t = time(NULL);
+	printf( "Util Time: %s\n", ctime(&t));
+	p = getpid();
+	pp  = getppid();
+	printf("Util PID: %d   PPID: %d \n", p, pp);
+	sleep(10);
+
+	t = time(NULL);
+
+	printf( "Util again: %s\n", ctime(&t));
+    clock_t  end   = clock();
+    double elapsed = (double) ( end - begin ) / CLOCKS_PER_SEC;
+    printf( "Time it took: %f\n", elapsed);
+    printf( "Begin: %ld\n", begin);
+    printf( "End: %ld\n", end);
+    exit(0);
+	}
+	
